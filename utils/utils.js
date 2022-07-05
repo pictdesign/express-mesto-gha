75e7f3e895eb8017d3fb7a3175318a5ce24c321e
pictdesign <ask@pict.design>
2022-07-05 12:03:15 +0300

    add: celebrate

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 const validateUrl = (url) => {
-  const regex = /^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:\/?#[\]@!$&'()*+,;=]{2,}#?$/g;
+  const regex = /^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/g;
   if (regex.test(url)) {
     return url;
   }
