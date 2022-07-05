099b253a47b452dd4cb74ebeabc2bf5144b28740
pictdesign <ask@pict.design>
2022-07-05 12:00:29 +0300

    add: celebrate

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { celebrate, Joi } = require('celebrate');
 const bodyParser = require('body-parser');
 const userRouter = require('./router/users');
 const cardsRouter = require('./router/cards');
@@ -15,8 +16,21 @@ mongoose.connect('mongodb://localhost:27017/mesto', {
 });
 
 app.use(bodyParser.json());
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom(validateUrl, 'custom validation'),
+  }),
+}), createUser);
 app.use(isAuthorized, userRouter);
 app.use(isAuthorized, cardsRouter);
 
---
+++ utils/utils.js
@@ -0,0 +1,7 @@
+module.exports.validateUrl = (url) => {
+  const regex = /^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:\/?#[\]@!$&'()*+,;=]{2,}#?$/g;
+  if (regex.test(url)) {
+    return url;
+  }
+  throw new Error('Invalid url');
+};
